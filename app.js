5f3623d6d4b6e12dcb68b1b385330e6aa08c7b33
FarhanAliShah <fsgraphicsdesigner@gmail.com>
2020-07-15 00:52:35 +0500

    Sigle way Communication Message Applcation

---
+++ app.js
@@ -0,0 +1,28 @@
+function addMessage(){
+    var val = document.getElementById("val")
+    var p = document.createElement("p");
+    p.style.background = "#F7E9DD";
+    p.style.margin = "10px";
+    p.style.fontSize = "18px";
+    p.style.fontFamily = "calibri";
+    p.style.padding = "5px";
+
+    var textNode =  document.createTextNode(val.value)
+    p.appendChild(textNode)
+    var messages = document.getElementById("messages");
+    messages.appendChild(p) 
+
+}
+
+let btnClear = document.querySelector('button');
+let inputs = document.querySelectorAll('input');
+btnClear.addEventListener('click',() => {
+    inputs.forEach(input => input.value = '');
+});
+
+// function addMessage(){
+//     if(document.getElementsByTagName('input').value == ""){
+//         alert('Please enter your message');
+//         return false;
+//     }
+// }
