d874eae653d555b727647d4696c8137067483a5c
SyedFarhanAliShah <47141248+SyedFarhanAliShah@users.noreply.github.com>
2020-10-01 15:59:43 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-function addMessage(){
-    var val = document.getElementById("val")
-    var p = document.createElement("p");
-    p.style.background = "#F7E9DD";
-    p.style.margin = "10px";
-    p.style.fontSize = "18px";
-    p.style.fontFamily = "calibri";
-    p.style.padding = "5px";
+// function addMessage(){
+//     var val = document.getElementById("val")
+//     var p = document.createElement("p");
+//     p.style.background = "#F7E9DD";
+//     p.style.margin = "10px";
+//     p.style.fontSize = "18px";
+//     p.style.fontFamily = "calibri";
+//     p.style.padding = "5px";
 
-    var textNode =  document.createTextNode(val.value)
-    p.appendChild(textNode)
-    var messages = document.getElementById("messages");
-    messages.appendChild(p) 
+//     var textNode =  document.createTextNode(val.value)
+//     p.appendChild(textNode)
+//     var messages = document.getElementById("messages");
+//     messages.appendChild(p) 
 
-}
 
 let btnClear = document.querySelector('button');
 let inputs = document.querySelectorAll('input');
@@ -20,9 +19,25 @@ btnClear.addEventListener('click',() => {
     inputs.forEach(input => input.value = '');
 });
 
-// function addMessage(){
-//     if(document.getElementsByTagName('input').value == ""){
-//         alert('Please enter your message');
-//         return false;
-//     }
-// }
+function addMessage(){
+    if(document.getElementById('val').value ==""){
+        alert('Please enter your message');
+        return false;
+
+    }
+    else
+    {
+        var val = document.getElementById("val");
+        var p = document.createElement("p");
+        p.style.background = "#F7E9DD";
+        p.style.margin = "10px";
+        p.style.fontSize = "18px";
+        p.style.fontFamily = "calibri";
+        p.style.padding = "5px";
+    
+        var textNode =  document.createTextNode(val.value)
+        p.appendChild(textNode)
+        var messages = document.getElementById("messages");
+        messages.appendChild(p) 
+    }
+}
